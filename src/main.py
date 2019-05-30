fa4780e42b81e52ba2afabea8cf0da54a4c71049
Ray Perry <rperry@runoranj.com>
2019-05-30 18:29:20 -0500

    Adhoc | Add docs and update README.

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# C950 - Ray Perry - rperr77 - 000981059
+
+def start_app():
+    """
+    This is the main entrypoint for the program.
+    """
+    print('Hello.');
+
+if __name__ == '__main__':
+    start_app();
+    
+# Local Variables:
+# compile-command: "python3 main.py"
+# End:
